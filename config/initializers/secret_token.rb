3e9fe141a7cbc2297e1041765aed3e1dc5532d3e
Rishi Shah <rishihahs@gmail.com>
2011-07-22 18:18:14 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colorswab::Application.config.secret_token = '38cad9cb1f4b34363d90cabafa5013ec8b6e83b38dab6a1e4d4f194bac9ad251d231d3cc723be4758da88cb4799f5ca4cb809f0362b3591f5882c4e8eb52219e'
