eba5df5f49714d09c20627997b24b12208d7583d
Rishi Shah <rishihahs@gmail.com>
2011-07-23 16:50:29 -0500

    Finished layout and logic

--- app/controllers/swab_controller.rb
+++ app/controllers/swab_controller.rb
@@ -1,14 +1,40 @@
 class SwabController < ApplicationController
   def index
-    orig = Color::RGB.from_html("#fff")
-    @colors = []
-    
-    4.times do |n|
-      @colors << orig.darken_by(100-(n*10))
+    if session[:color] or session[:colors]
+      @color = session[:color]
+      @colors = session[:colors]
+    else
+      swabs("#fff")
+      @color = @color.html
     end
   end
   
   def colors
-    
+    swabs(params[:color] || "#fff")
+    respond_to do |format|
+      format.html { 
+        session[:color] = @color.html
+        session[:colors] = @colors
+        redirect_to(swab_url)
+      }
+      format.js
+    end
   end
+  
+  private
+  
+    def swabs(color)
+      @color = Color::RGB.from_html(color)
+      @colors = []
+
+      4.times do |n|
+        @colors << @color.lighten_by((n*10)).html
+      end
+
+      @colors = @colors.drop(1)
+
+      4.times do |n|
+        @colors << @color.darken_by(100-(n*10)).html
+      end
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def copyright(year)
+    @now ||= Time.new.year
+    if year < @now
+      "#{year}-#{@now}"
+    else
+      "#{year}"
+    end
+  end
 end
